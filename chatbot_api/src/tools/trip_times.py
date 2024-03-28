1bce79549dd2e2d0d3220e69e7e0095236b0f294
Uche Okechukwu <uche.okechukwu@gmail.com>
2024-03-28 11:37:43 -0600

    added functionality to filter by name or iterate through dataframe to get_trip_time

--- chatbot_api/src/tools/trip_times.py
+++ chatbot_api/src/tools/trip_times.py
@@ -3,7 +3,7 @@ from typing import Any
 import numpy as np
 from langchain_community.graphs import Neo4jGraph
 import pandas as pd
-from chatbot_api.src.tools import geolocator
+from chatbot_api.src.tools import geodistance
 
 
 def _get_current_businesses() -> list[str]:
@@ -30,13 +30,29 @@ def _get_current_businesses() -> list[str]:
 def get_trip_time(start_location: str, business: str) -> (float, str):
     """Get the current trip time to businesses from a given location in seconds."""
     
-    current_businesses = _get_current_businesses()
-    b = current_businesses[current_businesses.name==business].iloc[0]
-
-    business_location = ' '.join([b['address'], b['city'], b['state'], b['postal_code']])                
+    if type(business) == str:
+        try:
+            current_businesses = _get_current_businesses()
+            b = current_businesses[current_businesses.name==business].iloc[0]
+        except:
+            return -1, f"{business} not found in the database."
+    else:
+        b = business
+    business_location = ' '.join([b['address'], b['city'], b['state'], b['postal_code']])               
     return geodistance.main(start_location=start_location,
                             end_location=business_location)
-    
-def get_nearest_business(start_location: str, current_businesses: pd.Series) -> dict[str, float]:
-    pass
-    return None # placeholder for dictionary of business_name, trip_time
+                            
+                            
+def get_nearest_business(start_location: str) -> dict[str, [float, float]]:
+    """Find the business with the shortest trip time."""
+    current_businesses = _get_current_businesses()
+
+    current_businesses["duration"], current_businesses["trip_time"] = zip(
+                        *current_businesses.apply(
+                        get_trip_time, axis=1, start_location=start_location))
+
+    shortest_time_idx = current_businesses["duration"].idxmin()
+    nearest_business = current_businesses['name'][shortest_time_idx]
+    shortest_duration = current_businesses['duration'][shortest_time_idx]
+
+    return {nearest_business: [shortest_duration]}
