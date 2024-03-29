7db84ea5d696e747e040a3784fcbb9baa115ce67
Uche Okechukwu <uche.okechukwu@gmail.com>
2024-03-28 19:04:20 -0600

    completed streamlit front end 🚀🚀🚀🚀🚀🚀

---
+++ chatbot_frontend/src/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run any setup steps or pre-processing tasks here
+echo "Staring yelp chatbot frontend"
+
+# Run the streamlit script
+streamlit run main.py
--- tests/async_agent_requests.py
+++ tests/async_agent_requests.py
@@ -14,24 +14,18 @@ async def make_bulk_requests(url, data):
     tasks = [make_async_post(url, payload) for payload in data]
     responses = await asyncio.gather(*tasks)
     outputs = [r.json()["output"] for r in responses]
-    # outputs = []
-    # for r in responses:
-    #     if r.content:
-    #         try:
-    #             output = r.json()["output"]
-    #             outputs.append(output)
-    #         except Exception as e:
-    #             print(str(e))
-    #     else:
-    #         print("Empty response content")
+
     return outputs
     
 questions=[
-    "What are users saying about wedding venues in Illinois?",
-    "What businesses get the most reviews in New York?",
-    "How many sports centers are in California?",
-    "Where do people think were the best party venues in 2022?",
-    "Where do people find cheap clothes in Texas?"
+    "What category of businesses get the most reviews?"
+    "How state has the smallest reviewed department stores?"
+    "Which city has the most recreation facilities?"
+    # "What are users saying about wedding venues in Illinois?",
+    # "What businesses get the most reviews in New York?",
+    # "How many sports centers are in California?",
+    # "Where do people think were the best party venues in 2022?",
+    # "Where do people find cheap clothes in Texas?"
 ]
 request_bodies = [{"text": q} for q in questions]
 start_time = time.perf_counter()
